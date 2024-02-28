32b9fca560b452f1cea78b4e10f120e8543924b8
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 19:08:37 +0400

    created settings page with subscription button

---
+++ pages/settings/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+definePageMeta({ middleware: "auth", layout: "dashboard" });
+useHead({
+  title: "AI companion | Settings",
+});
+const { data: isPro } = useNuxtData("isPro");
+</script>
+
+<template>
+  <div class="h-full p-4 space-y-2">
+    <h3 class="text-lg font-medium">Settings</h3>
+    <div class="text-muted-foreground text-sm">
+      {{
+        isPro
+          ? "You are currently on a Pro plan."
+          : "You are currently on a free plan."
+      }}
+    </div>
+    <SubscriptionButton :isPro="isPro" />
+  </div>
+</template>
