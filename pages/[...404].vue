7608f83a6c586f0133001edd04b3af449287bf72
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 23:59:50 +0400

    created 404 page

--- pages/[...404].vue
+++ pages/[...404].vue
@@ -1,7 +1,8 @@
 <script setup>
 import { Button } from "~/components/ui/button";
+definePageMeta({ layout: "auth" });
 useHead({
-  title: "AI companion | 404",
+  title: "AI genius | 404",
 });
 </script>
 
@@ -9,29 +10,17 @@ useHead({
   <div
     class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16"
   >
-    <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-      <div class="relative">
-        <div class="absolute">
-          <div class="">
-            <h1 class="my-2 text-gray-800 font-bold text-2xl">
-              Looks like you've found the doorway to the great nothing
-            </h1>
-            <p class="my-2 text-gray-800">
-              Sorry about that! Please visit our hompage to get where you need
-              to go.
-            </p>
-            <NuxtLink to="/" class="mt-2 mx-auto">
-              <Button variant="primary">BACK TO HOMEPAGE</Button>
-            </NuxtLink>
-          </div>
+    <div class="w-full lmd:w-1/2 relative pb-12 lg:pb-0">
+      <div class="relative flex items-center flex-col">
+        <div class="mb-8 mx-auto">
+          <img src="https://i.ibb.co/G9DC8S0/404-2.png" class="mx-auto" />
         </div>
-        <div>
-          <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
-        </div>
+        <NuxtLink to="/" class="mx-auto">
+          <Button variant="primary" class="text-white text-lg"
+            >BACK TO HOMEPAGE</Button
+          >
+        </NuxtLink>
       </div>
-    </div>
-    <div>
-      <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
     </div>
   </div>
 </template>
