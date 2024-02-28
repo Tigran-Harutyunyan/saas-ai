0e2d4aad8b12e11c46a6f72e120a2372b795d8e1
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 11:36:51 +0400

    created Navbar component
    it hosts MobileSidebar component

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { UserButton } from "vue-clerk";
+import MobileSidebar from "@/components/MobileSidebar.vue";
+
+interface NavbarProps {
+  isPro?: boolean;
+}
+const { isPro } = defineProps<NavbarProps>();
+</script>
+
+<template>
+  <div class="flex items-center p-4">
+    <ClientOnly>
+      <MobileSidebar :isPro="isPro" />
+    </ClientOnly>
+    <div class="flex w-full justify-end">
+      <UserButton after-sign-out-url="/" />
+    </div>
+  </div>
+</template>
