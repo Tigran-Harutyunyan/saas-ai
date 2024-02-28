c866998ea9ca094fe22ab916559c52942ca4e28c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 11:42:02 +0400

    created Heading component for pages

---
+++ components/Heading.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+
+interface HeadingProps {
+  title: string;
+  description: string;
+  icon: any;
+  iconColor?: string;
+  bgColor?: string;
+}
+
+const {
+  title,
+  description,
+  icon: Icon,
+  iconColor,
+  bgColor,
+} = defineProps<HeadingProps>();
+</script>
+
+<template>
+  <div class="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+    <div :class="cn('p-2 w-fit rounded-md', bgColor)">
+      <component :is="icon" :class="cn('w-10 h-10', iconColor)" />
+    </div>
+    <div>
+      <h2 class="text-3xl font-bold">{{ title }}</h2>
+      <p class="text-sm text-muted-foreground">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
