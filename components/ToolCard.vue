844337f6c0e433babe23fafe99e7ffbe6a0c13bf
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 11:52:57 +0400

    created ToolCard component

---
+++ components/ToolCard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+
+const router = useRouter();
+
+interface ToolCardProps {
+  src: string;
+  href: string;
+  title: string;
+  description: string;
+  premium?: boolean;
+}
+const { src, href, title, description, premium } = defineProps<ToolCardProps>();
+
+const onClick = () => {
+  router.push(href);
+};
+</script>
+
+<template>
+  <Card @click="onClick" class="group cursor-pointer">
+    <CardHeader>
+      <CardTitle class="text-lg font-bold flex items-center">
+        <div
+          class="relative h-8 w-8 mr-2 group-hover:scale-125 transition duration-150"
+        >
+          <NuxtImg alt="Icon" :src="src" fill />
+        </div>
+        {{ title }}
+      </CardTitle>
+      <CardDescription>{{ description }}</CardDescription>
+
+      <CardContent class="p-0" v-if="premium">
+        <Badge variant="premium" class="uppercase">pro</Badge>
+      </CardContent>
+    </CardHeader>
+  </Card>
+</template>
