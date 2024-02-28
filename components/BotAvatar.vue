69c77a23f5f39c473cec9f1f75bbcd7b840efc6c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 11:38:23 +0400

    created User and Bot avatars

---
+++ components/BotAvatar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+</script>
+
+<template>
+  <Avatar class="h-8 w-8">
+    <AvatarImage src="/images/logo.png" class="p-1" />
+  </Avatar>
+</template>
---
+++ components/UserAvatar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { useClerkProvide } from "vue-clerk";
+const { state } = useClerkProvide();
+</script>
+
+<template>
+  <Avatar class="h-12 w-12">
+    <AvatarImage :src="state?.user?.imageUrl!" />
+  </Avatar>
+</template>
