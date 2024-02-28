780d808858f309f9fd1cb643e7e0df395eb49f0c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 11:46:46 +0400

    created subscription lib to query user subscription

---
+++ lib/subscription.ts
@@ -0,0 +1,32 @@
+import prismadb from "@/lib/prismadb";
+
+const DAY_IN_MS = 86_400_000;
+
+export const checkSubscription = async (userId: string) => {
+
+  if (!userId) {
+    return false;
+  }
+
+  const userSubscription = await prismadb.userSubscription.findUnique({
+    where: {
+      userId,
+    },
+    select: {
+      stripeSubscriptionId: true,
+      stripeCurrentPeriodEnd: true,
+      stripeCustomerId: true,
+      stripePriceId: true,
+    },
+  })
+
+  if (!userSubscription) {
+    return false;
+  }
+
+  const isValid =
+    userSubscription.stripePriceId &&
+    userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now()
+
+  return !!isValid;
+};
