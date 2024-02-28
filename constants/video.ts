ee16df295cf939770b8dd7afe9a5e4723315760c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 23:18:28 +0400

    created constants for pages

---
+++ constants/video.ts
@@ -0,0 +1,10 @@
+import * as z from "zod";
+import { toTypedSchema } from "@vee-validate/zod";
+
+export const formSchema = toTypedSchema(
+  z.object({
+    prompt: z.string().min(1, {
+      message: "Prompt is required."
+    }),
+  })
+);
