94cdf9ffd617f69ba69ec129cc0314570d67fc6f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-28 11:41:17 +0400

    created Empty component for no results case

---
+++ components/Empty.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface EmptyProps {
+  label: string;
+}
+
+const { label } = defineProps<EmptyProps>();
+</script>
+
+<template>
+  <div class="h-full p-20 flex flex-col items-center justify-center">
+    <div class="relative h-72 w-72">
+      <NuxtImg src="/images/empty.png" alt="Empty" />
+    </div>
+    <p class="text-muted-foreground text-sm text-center">
+      {{ label }}
+    </p>
+  </div>
+</template>
