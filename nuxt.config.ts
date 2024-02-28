5fe13d9f5bce2771a3f99b7ef80a44c3d892f9e7
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-29 00:06:00 +0400

    new packages and configs

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt-alt/markdown-it', '@nuxtjs/google-fonts', '@fixers/nuxt-stripe', '@pinia/nuxt', "@nuxt/image", '@vueuse/nuxt', '@vee-validate/nuxt'],
+  build: {
+    transpile: ['vue-clerk', '@clerk/clerk-js'],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  alias: {
+    ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+  },
+  runtimeConfig: {
+    public: {
+      clerkPublishableKey: process.env.NUXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+      appUrl: process.env.NUXT_PUBLIC_APP_URL,
+      unsplashAccessKey: process.env.NUXT_PUBLIC_UNSPLASH_ACCESS_KEY,
+      crisp: process.env.CRISP
+    },
+    clerkSecretKey: process.env.CLERK_SECRET_KEY,
+    stripeKey: process.env.STRIPE_API_KEY,
+    stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET
+  },
+  googleFonts: {
+    subsets: 'latin',
+    families: {
+      Poppins: {
+        wght: [200, 300, 400, 500, 600, 700, 800, 900],
+      },
+      Montserrat: {
+        wght: [600],
+      },
+    }
+  },
+  markdownit: {
+    runtime: true,
+    use: ['markdown-it-highlightjs']
+  }
+
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-const animate = require("tailwindcss-animate")
+const animate = require("tailwindcss-animate");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   safelist: ["dark"],
-  
+
   theme: {
     container: {
       center: true,
@@ -63,22 +63,12 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-        "collapsible-down": {
-          from: { height: 0 },
-          to: { height: 'var(--radix-collapsible-content-height)' },
-        },
-        "collapsible-up": {
-          from: { height: 'var(--radix-collapsible-content-height)' },
-          to: { height: 0 },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "collapsible-down": "collapsible-down 0.2s ease-in-out",
-        "collapsible-up": "collapsible-up 0.2s ease-in-out",
       },
     },
   },
   plugins: [animate],
-}
+};
